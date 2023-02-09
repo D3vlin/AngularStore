b3baab38518de207c36a28ac83463c70698b752b
D3vlin <alexisdq16@gmail.com>
2023-02-09 13:10:26 -0500

    added product component

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,5 +1,7 @@
 import { Component } from '@angular/core';
 
+import { Product } from './models/product.model';
+
 @Component({
   selector: 'app-root',
   templateUrl: './app.component.html',
@@ -8,7 +10,34 @@ import { Component } from '@angular/core';
 export class AppComponent {
   imgParent = '';
 
+  products: Product[] = [
+    {
+      id: 1,
+      name: 'product1',
+      img: '../assets/images/toy.jpg',
+      price: 0
+    },
+    {
+      id: 2,
+      name: 'product2',
+      img: '../assets/images/toy.jpg',
+      price: 0
+    },
+    {
+      id: 3,
+      name: 'product3',
+      img: '../assets/images/toy.jpg',
+      price: 0
+    },
+    {
+      id: 4,
+      name: 'product4',
+      img: '../assets/images/toy.jpg',
+      price: 0
+    }
+  ];
+
   onLoaded(img: string) {
-    console.log('-appLog: ', img)
+    console.log('-appLog: ', img);
   }
 }
