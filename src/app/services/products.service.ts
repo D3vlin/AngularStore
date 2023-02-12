46fb8eedcd78be9cd17d487f9547ba53654ee19f
D3vlin <alexisdq16@gmail.com>
2023-02-12 08:37:00 -0500

    added retry query

--- src/app/services/products.service.ts
+++ src/app/services/products.service.ts
@@ -3,6 +3,7 @@ import { Injectable } from '@angular/core';
 import { HttpClient, HttpParams } from '@angular/common/http'
 
 import { UpdateProductDTO, CreateProductDTO, Product } from '../models/product.model';
+import { retry } from "rxjs/operators";
 
 @Injectable({
   providedIn: 'root'
@@ -21,7 +22,7 @@ export class ProductsService {
       params = params.set('limit', limit);
       params = params.set('offset', offset);
     }
-    return this.httpClient.get<Product[]>(this.apiUrl), {params};
+    return this.httpClient.get<Product[]>(this.apiUrl, {params}).pipe(retry(3));
   }
 
   getProduct(id: string) {
