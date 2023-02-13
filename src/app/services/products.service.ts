6c4f6ee14abef3955841c0e6700e84423bdac592
D3vlin <alexisdq16@gmail.com>
2023-02-13 14:43:00 -0500

    added interceptor context

--- src/app/services/products.service.ts
+++ src/app/services/products.service.ts
@@ -2,11 +2,13 @@ import { Injectable } from '@angular/core';
 
 import { HttpClient, HttpParams, HttpErrorResponse, HttpStatusCode } from '@angular/common/http'
 
-import { UpdateProductDTO, CreateProductDTO, Product } from '../models/product.model';
+import { UpdateProductDTO, CreateProductDTO, Product } from './../models/product.model';
 import { retry, catchError, map } from "rxjs/operators";
 import { throwError } from "rxjs";
 
 import { environment } from "./../../environments/environment";
+
+import { CheckTime } from "./../interceptors/time.interceptor";
 
 @Injectable({
   providedIn: 'root'
@@ -58,7 +60,12 @@ export class ProductsService {
   }
 
   getProductsByPage(limit: number, offset: number) {
-    return this.httpClient.get<Product[]>(`${this.apiUrl}`, { params: { limit, offset }}).pipe(
+    let params = new HttpParams();
+    if(limit && offset) {
+      params = params.set('limit', limit);
+      params = params.set('offset', offset);
+    }
+    return this.httpClient.get<Product[]>(`${this.apiUrl}`, { params, context: CheckTime() }).pipe(
       map(products => products.map(item => {
         return {
           ...item,
