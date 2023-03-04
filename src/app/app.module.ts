0ce4986251194ba19995a65de28c24a3b7bb89e1
D3vlin <alexisdq16@gmail.com>
2023-03-04 12:21:50 -0500

    added category module

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -1,4 +1,5 @@
 import { NgModule } from '@angular/core';
+import { CommonModule } from '@angular/common';
 import { BrowserModule } from '@angular/platform-browser';
 import { FormsModule } from '@angular/forms';
 import { HttpClientModule, HTTP_INTERCEPTORS} from '@angular/common/http'
@@ -15,6 +16,7 @@ import { NotFoundComponent } from './not-found/not-found.component';
     NotFoundComponent
   ],
   imports: [
+    CommonModule,
     BrowserModule,
     AppRoutingModule,
     FormsModule,
--- src/app/website/website-routing.module.ts
+++ src/app/website/website-routing.module.ts
@@ -3,7 +3,6 @@ import { RouterModule, Routes } from '@angular/router';
 
 import { HomeComponent } from './pages/home/home.component';
 import { LayoutComponent } from './components/layout/layout.component';
-import { CategoryComponent } from './pages/category/category.component';
 import { MyCartComponent } from './pages/my-cart/my-cart.component';
 import { LoginComponent } from './pages/login/login.component';
 import { RegisterComponent } from './pages/register/register.component';
@@ -17,7 +16,7 @@ const routes: Routes = [{
   children: [
     { path: '', redirectTo: 'home', pathMatch: 'full' },
     { path: 'home', component: HomeComponent },
-    { path: 'category/:id', component: CategoryComponent },
+    { path: 'category', loadChildren: () => import('./pages/category/category.module').then( m => m.CategoryModule ) },
     { path: 'product/:id', component: ProductDetailComponent },
     { path: 'myCart', component: MyCartComponent },
     { path: 'login', component: LoginComponent },
