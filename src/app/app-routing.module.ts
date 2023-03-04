898561dc890a2f642054aa3992b4d0a078ef0c8d
D3vlin <alexisdq16@gmail.com>
2023-03-04 09:46:35 -0500

    added cms module

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -28,6 +28,7 @@ const routes: Routes = [
       { path: 'profile', component: ProfileComponent },
     ],
   },
+  { path: 'cms', loadChildren: () => import('./cms/cms.module').then( m => m.CmsModule ) },
   { path: '**', component: NotFoundComponent },
 ];
 
